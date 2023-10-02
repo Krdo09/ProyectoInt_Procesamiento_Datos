643c7af92827eaae14041779f7628be3ae0a5a3b
Julián Agudelo <agudelojulian649@gmail.com>
2023-10-02 12:24:56 -0500

    Se realiza parte III del proyecto integrador. Se implementa función para verificación de tipos de datos. Se implementa función para determinar el número de fumadores por genero

--- OneDrive/Documentos/Documentos Julián/Clases ProTalento/Trabajos/Procesamiento_De_Datos/Proyecto_integrador/code.py
+++ OneDrive/Documentos/Documentos Julián/Clases ProTalento/Trabajos/Procesamiento_De_Datos/Proyecto_integrador/code.py
@@ -25,3 +25,29 @@ def separation(df: pd.DataFrame):
 def average_age(df: pd.DataFrame) -> pd.DataFrame:
     mean_age = df['age'].mean()
     return mean_age
+
+
+# Parte III
+def data_check(df: pd.DataFrame):
+    columns_names = ['age', 'has_anaemia', 'creatinine_phosphokinase_concentration_in_blood',
+              'has_diabetes', 'heart_ejection_fraction', 'has_high_blood_pressure',
+              'platelets_concentration_in_blood', 'serum_creatinine_concentration_in_blood',
+              'serum_sodium_concentration_in_blood', 'is_male', 'is_smoker', 'days_in_study',
+              'is_dead']
+    for column in columns_names:
+        data_type = str(df[column].dtype)
+        if data_type == 'int68':
+            df[column] = pd.to_numeric(df[column], downcast='integer')
+
+        if data_type == 'float64':
+            df[column] = pd.to_numeric(df[column], downcast='float')
+
+        if data_type == 'bool':
+            df[column] = df[column].astype(bool)
+
+def smoker(df: pd.DataFrame):
+    smokers = df.groupby(['is_male', 'is_smoker']).size().reset_index(name='count')
+    smokers = smokers[smokers['is_smoker'] == True]
+    smokers.columns = ['is_male', 'is_smoker', 'number_smokers']
+    return smokers
+
--- OneDrive/Documentos/Documentos Julián/Clases ProTalento/Trabajos/Procesamiento_De_Datos/Proyecto_integrador/pruebas.py
+++ OneDrive/Documentos/Documentos Julián/Clases ProTalento/Trabajos/Procesamiento_De_Datos/Proyecto_integrador/pruebas.py
@@ -1,1 +1,7 @@
+from code import *
+
+prueba = to_df(data)
+data_check(prueba)
+print(smoker(prueba))
+
 
